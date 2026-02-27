159e90ff368352ff31a58cb6c64bb3634d04f8c9
Lavanya <lavsd371@gmail.com>
2026-02-27 16:21:54 +0530

    Day-10

--- student.java
+++ student.java
@@ -1,9 +1,9 @@
-class Student {
+class student {
     String name;
     int id;
 
     // Constructor using 'this' keyword
-    Student(String name, int id) {
+    student(String name, int id) {
         this.name = name;
         this.id = id;
     }
@@ -13,7 +13,7 @@ class Student {
     }
 
     public static void main(String[] args) {
-        Student s = new Student("Alice", 101);
+        student s = new student("Alice", 101);
         s.display();
     }
 }
